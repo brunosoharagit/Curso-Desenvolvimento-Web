31b9853ebfeb69e90c02d1bb8f16b6b26c1737ea
brunosoharagit <b.sohara.git@gmail.com>
2021-03-09 16:25:41 -0300

    CDSV2021.03.09.16.25 - Remoção de linha (tipagem.js)

--- seção 1 - fundamentos javascript/tipos de dados/tipagem.js
+++ seção 1 - fundamentos javascript/tipos de dados/tipagem.js
@@ -3,8 +3,3 @@ console.log("Resultado: " + qualquer + " -- tipo: " + typeof qualquer)
 
 qualquer = 5
 console.log("Resultado: " + qualquer +" -- tipo: " + typeof qualquer)
-
-function escreverMensagem(msg){
-    msg = "Olá mundo!!"
-    console.log(msg)
-}
