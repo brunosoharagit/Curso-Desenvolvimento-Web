d1efc462d70a770dbe5e06ea9357207dc03eeb8f
brunosoharagit <b.sohara.git@gmail.com>
2021-03-10 09:47:45 -0300

    CDSV2021.03.10.09.47 - templateStrings.js > arrow function

--- seção 1 - fundamentos javascript/tipos de dados/templateStrings.js
+++ seção 1 - fundamentos javascript/tipos de dados/templateStrings.js
@@ -8,5 +8,6 @@ const templateStrings = `Olá ${nome}!`
 console.log(templateStrings)
 console.log(`2 + 2 = ${2 + 2}`)
 
+//Template Strings em função
 const up = texto => texto.toUpperCase()
 console.log(`Ei... ${up('cuidado')}!`)
