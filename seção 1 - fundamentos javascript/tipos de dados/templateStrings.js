591f9c8993c2cf9c8bf30cc43f74cd03b2c2f21a
brunosoharagit <b.sohara.git@gmail.com>
2021-03-10 09:43:45 -0300

    CDSV2021.03.10.09.43 - edição do arquivo templateStrings.js

---
+++ seção 1 - fundamentos javascript/tipos de dados/templateStrings.js
@@ -0,0 +1,12 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+
+console.log(concatenacao)
+
+const templateStrings = `Olá ${nome}!`
+
+console.log(templateStrings)
+console.log(`2 + 2 = ${2 + 2}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
