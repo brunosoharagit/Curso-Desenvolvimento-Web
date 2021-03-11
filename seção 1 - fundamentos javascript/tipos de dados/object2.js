23f1d87ad05d583afa640435da86be536434f371
brunosoharagit <b.sohara.git@gmail.com>
2021-03-11 10:12:11 -0300

    CDSV2021.03.11.10.12 - object2.js: criação e edição

---
+++ seção 1 - fundamentos javascript/tipos de dados/object2.js
@@ -0,0 +1,18 @@
+const computador = {
+    monitor: 'AOC 21 inch. 144hz',
+    mouse: 'Razer Deathadder 3500 dpi',
+    teclado: 'Mecânico Gamer Redragon Kumara',
+    acessorios: {
+        headset: 'Husky Polar Gamer, Stereo',
+        mousepad: 'Havit Control Extra-Grande',
+        microfone: 'HyperX QuadCast',
+        suporte: 'Redragon Scepter PRO RGB'
+    },
+    CPU: {
+        gabinete: 'Pichau Lark - Vidro',
+        cooler: 'Water Cooler NZXT Kraken',
+        processador: 'Intel core i9-9900',
+        placaMae: 'Gigabyte Z490 Aorus Xtreme',
+        memoria: 'Corsair Vengeance RGB PRO 32GB',
+    }
+}
