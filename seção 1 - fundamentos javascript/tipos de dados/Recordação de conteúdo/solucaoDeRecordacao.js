8482fff527b256a89cf966879126e3ccb033b104
brunosoharagit <b.sohara.git@gmail.com>
2021-03-11 16:46:30 -0300

    CDSV2021.03.11.16.46 - solucaoDeRecordacao: edicao do arquivo

--- seção 1 - fundamentos javascript/tipos de dados/Recordação de conteúdo/solucaoDeRecordacao.js
+++ seção 1 - fundamentos javascript/tipos de dados/Recordação de conteúdo/solucaoDeRecordacao.js
@@ -14,34 +14,19 @@ function calcualrIMC(peso, altura){
     const IMC = peso / (Math.pow(altura, 2))
 
     if (IMC <= 18.5){
-        console.log(`Peso: ${peso}kg. 
-Altura: ${altura}m.
-Valor IMC: ${IMC.toFixed(2)}
-Resultado: abaixo do peso.`)
+        console.log(`Peso: ${peso}kg. -- Altura: ${altura}m. -- Valor IMC: ${IMC.toFixed(2)} -- Resultado: abaixo do peso.`)
     }
     else if (IMC > 18.5 && IMC <= 24.9){
-        console.log(`Peso: ${peso}kg. 
-Altura: ${altura}m.
-Valor IMC: ${IMC.toFixed(2)}
-Resultado: peso normal.`)
+        console.log(`Peso: ${peso}kg. -- Altura: ${altura}m. -- Valor IMC: ${IMC.toFixed(2)} -- Resultado: peso normal.`)
     }
     else if (IMC > 25 && IMC <= 29.9){
-        console.log(`Peso: ${peso}kg. 
-Altura: ${altura}m.
-Valor IMC: ${IMC.toFixed(2)}
-Resultado: obesidade grau 1.`)
+        console.log(`Peso: ${peso}kg. -- Altura: ${altura}m. -- Valor IMC: ${IMC.toFixed(2)} -- Resultado: obesidade grau 1.`)
     }
     else if (IMC > 34 && IMC <= 39.9){
-        console.log(`Peso: ${peso}kg. 
-Altura: ${altura}m.
-Valor IMC: ${IMC.toFixed(2)}
-Resultado: obesidade grau 2.`)
+        console.log(`Peso: ${peso}kg. -- Altura: ${altura}m. -- Valor IMC: ${IMC.toFixed(2)} -- Resultado: obesidade grau 2.`)
     }
     else {
-        console.log(`Peso: ${peso}kg. 
-Altura: ${altura}m.
-Valor IMC: ${IMC.toFixed(2)}
-Resultado: obesidade grau 3.`)
+        console.log(`Peso: ${peso}kg. -- Altura: ${altura}m. -- Valor IMC: ${IMC.toFixed(2)} -- Resultado: obesidade grau 3.`)
     }
 }
 
