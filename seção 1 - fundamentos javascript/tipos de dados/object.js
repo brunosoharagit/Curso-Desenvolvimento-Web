5a66222289e5bad285bba2f67c1c41a6ce07b578
brunosoharagit <b.sohara.git@gmail.com>
2021-03-11 10:11:29 -0300

    CDSV20221.03.11.10.11 - object.js: adição e edição

---
+++ seção 1 - fundamentos javascript/tipos de dados/object.js
@@ -0,0 +1,25 @@
+//JSON: formato textual
+
+const aluno = {
+    nome: 'Bruno',
+    ra: '68612',
+    ano: '3º',
+    disciplinasCadastradas: ['Matemática', 
+    'Física', 
+    'Português', 
+    'História', 
+    'Geografia', 
+    'Filosofia e Sociologia',
+    'Estudos literários']
+}
+
+console.log(aluno)
+console.log("Nome do aluno cadastrado: " + aluno.nome)
+console.log("RA do aluno cadastrado: " + aluno.ra)
+
+const frutas = [
+    {fruta: 'Limão', preco: 3.50, registro: '3d44fe1a0'},
+    {fruta: 'Maçã', preco: 2.30, registro: '99da60c6b'},
+    {fruta: 'Banana', preco: 4.50, registro: '1d33eac65'}
+]
+
