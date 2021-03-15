a76b86a5970b9ab6c60dc96414711f22598406c8
brunosoharagit <b.sohara.git@gmail.com>
2021-03-15 11:04:13 -0300

    CDSV2021.03.15.11.03 - funcoes.js: adição de funções com e sem retorno

--- seção 1 - fundamentos javascript/tipos de dados/funcoes.js
+++ seção 1 - fundamentos javascript/tipos de dados/funcoes.js
@@ -4,3 +4,20 @@ class Produto{}
 console.log(typeof Produto)
 
 //função sem retorno
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(2, 3, 4, 5, 10, 15, 20, 25, 30)
+imprimirSoma()
+
+//função com retorno
+function soma(a, b = 0){
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
+//console.log(soma("Olá", " mundo!!"))
