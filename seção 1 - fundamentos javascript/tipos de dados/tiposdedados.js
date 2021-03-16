aad6abd27363bf14334a2d3ca7726f3e66fbdcdc
brunosoharagit <b.sohara.git@gmail.com>
2021-03-16 16:28:00 -0300

    CDSV2021.03.16.16.27 - tiposdedados.js: inserindo tipos de dados

---
+++ seção 1 - fundamentos javascript/tipos de dados/tiposdedados.js
@@ -0,0 +1,31 @@
+{
+    {
+        {
+            {
+                var sera = 'Será????'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+var numero = "4"
+numero = 4
+console.log(numero)
+
+function teste() {
+    var local = 192
+}
+
+// teste()
+// console.log(local)
+
+//sobrescrever valores
+//Existem dois tipos de valores
+//  a) Global: visível para todos
+//  b) Variável 'var' em escopo de função: é visível apenas na função 
+
+var a = 5
+var a = 'teste'
+
+window.a
