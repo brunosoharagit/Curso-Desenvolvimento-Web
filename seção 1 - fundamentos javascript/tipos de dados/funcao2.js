68146858ae0271a8a8e29a97bba65cb86f67e3c1
brunosoharagit <b.sohara.git@gmail.com>
2021-03-16 09:55:01 -0300

    CDSV2021.03.16.09.54 - funcao2.js: inserindo arrow function e retorno implicito

---
+++ seção 1 - fundamentos javascript/tipos de dados/funcao2.js
@@ -0,0 +1,17 @@
+//Armazenando uma função em variável 
+const funcaoSoma = function (a, b){
+    return a + b
+}
+
+console.log(funcaoSoma(2, 3))
+
+
+//Arrow Function
+const funcaoSubt = (a, b) => {return a - b}
+
+console.log(funcaoSubt(3, 2))
+
+//Retorno implícito
+const funcaoMult = (a, b) => a * b
+
+console.log(funcaoMult(3, 2))
