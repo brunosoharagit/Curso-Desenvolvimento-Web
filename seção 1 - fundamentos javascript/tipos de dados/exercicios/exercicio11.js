88332ec4df2c2aa5a62031f57a30e6402b21920d
brunosoharagit <b.sohara.git@gmail.com>
2021-03-18 15:24:21 -0300

    CDSV2021.03.18.15.24 - exercicio11.js: função 'anoBissexto'

---
+++ seção 1 - fundamentos javascript/tipos de dados/exercicios/exercicio11.js
@@ -0,0 +1,15 @@
+function anoBissexto(ano){
+    if (ano % 4 === 0){
+        return true
+    }
+    else if (ano % 400 === 0){
+        return true
+    }
+    else if (ano % 100 === 0){
+        return false
+    }
+    else{
+        return false
+    }
+}
+
