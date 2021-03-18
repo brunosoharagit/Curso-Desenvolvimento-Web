83c52c5df1da764c7e6d952bea0666b31eab79f8
brunosoharagit <b.sohara.git@gmail.com>
2021-03-18 09:25:30 -0300

    CDSV2021.03.18.09.25 - loopsVar.js: criando casos com repetição 'for'

---
+++ seção 1 - fundamentos javascript/tipos de dados/loopsVar.js
@@ -0,0 +1,26 @@
+
+/*incremento e decrimento
+a) incremento = quando aumenta
+sintaxe: i++ || i+=1 || i = i + 1
+
+b) decrimento = quando diminui
+sintaxe: i-- || i-=1 || i = i - 1*/
+
+console.log("Repetição com incremento")
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+
+console.log("\nRepetição com decrimento")
+for (var i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+/* 
+for (início, fim, intervalo){
+    comando a ser inserido...
+}
+ * 
+ */
+
