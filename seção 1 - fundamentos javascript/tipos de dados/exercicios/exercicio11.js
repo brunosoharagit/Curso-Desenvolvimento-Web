c52915b0964df046fe4c31108fb93d027a6a0931
brunosoharagit <b.sohara.git@gmail.com>
2021-03-18 16:02:17 -0300

    CDSV2021.03.18.16.01 - exercicio11.js: corrigindo problemas

--- seção 1 - fundamentos javascript/tipos de dados/exercicios/exercicio11.js
+++ seção 1 - fundamentos javascript/tipos de dados/exercicios/exercicio11.js
@@ -1,21 +1,18 @@
-function fatorial(valor) {
-    // para valores negativos
-    if(valor<0) {
+function fatorial(numero){
+    if (numero < 0){
+        return `Número inválido!!`
+    }
+    else if (numero == 0 || numero == 1){
+        return 1
+    }
+    else {
+        var count = 1
+        for (var x=numero; x>1; x--){
+            count = count * x;
+        }
 
-      return 'Valor deve ser maior ou igual a zero';
-    
-      // para valor = 0  ou igual a 1
-    } else if ( (valor == 0) || (valor == 1) ) {
-
-      return 1;
-     
-    } else {
-
-      var acumula = 1;
-      for(x=valor;x>1;x--) {
-        acumula = acumula * x;
-      }
-      return acumula;
-    } 
-
+        return count;
+    }
 }
+
+console.log(fatorial(5))
