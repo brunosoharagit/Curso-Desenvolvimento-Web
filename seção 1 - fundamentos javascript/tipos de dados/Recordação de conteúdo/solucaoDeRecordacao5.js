91ad627bcc43fb216a8f10dca75c44b2a3f3c9d7
brunosoharagit <b.sohara.git@gmail.com>
2021-03-19 10:25:19 -0300

    CDSV2021.03.19.10.24 - solucaoDeRecordacao5.js: codificando conteudos

---
+++ seção 1 - fundamentos javascript/tipos de dados/Recordação de conteúdo/solucaoDeRecordacao5.js
@@ -0,0 +1,19 @@
+var numero = 3
+
+function imprimeNumero(numero){
+    var numero = 5
+    console.log(numero)
+}
+
+console.log(numero)
+imprimeNumero(numero)
+
+var numero2 = 3
+
+function imprimeNumero(numero2){
+    let numero2 = 5
+    console.log(numero2)
+}
+
+console.log(numero2)
+imprimeNumero(numero2)
