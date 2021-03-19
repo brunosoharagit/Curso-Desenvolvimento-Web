d2d0f31f24aca323c160d3ef5fdd7a93931e7762
brunosoharagit <b.sohara.git@gmail.com>
2021-03-19 12:07:27 -0300

    CDSV2021.03.19.12.08 - atribuicao.js: operacao de atribuicao

---
+++ seção 1 - fundamentos javascript/tipos de dados/atribuicao.js
@@ -0,0 +1,18 @@
+//operadores de atribuição
+const a = 9
+let b = 3
+
+b += a
+console.log(b)
+
+b -= a
+console.log(b)
+
+b *= a
+console.log(b)
+
+b /= a
+console.log(b.toPrecision(2))
+
+b %= a
+console.log(b)
