ae09596747ab1aab32b993aaa3b865f70e76685d
brunosoharagit <b.sohara.git@gmail.com>
2021-03-19 10:26:58 -0300

    CDSV2021.03.19.10.27 - loopsVar2.js: adicionando mais casos

--- seção 1 - fundamentos javascript/tipos de dados/loopsVar2.js
+++ seção 1 - fundamentos javascript/tipos de dados/loopsVar2.js
@@ -6,9 +6,9 @@ for (var i = 0; i <= 10; i++){
     })
 }
 
-funcao[0]()
-funcao[2]()
-funcao[4]()
+funcao[0]() //11
+funcao[2]() //11
+funcao[4]() //11
 
 /*
  * Retorna o mesmo valor para todos os índices
