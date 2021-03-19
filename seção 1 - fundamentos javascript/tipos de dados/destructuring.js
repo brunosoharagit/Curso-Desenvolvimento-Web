5a9b0beae41f2078d89ed26d6e700b21470c1a4d
brunosoharagit <b.sohara.git@gmail.com>
2021-03-19 12:08:15 -0300

    CSV2021.03.19.12.08 - destructuring.js: criacao do arquivo

---
+++ seção 1 - fundamentos javascript/tipos de dados/destructuring.js
@@ -0,0 +1,21 @@
+/* Parte 1
+ * 
+ *  
+ */
+const pessoa = {
+    nome: 'Gerson',
+    idade: 45,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 123
+    }
+}
+
+const { nome, idade } = pessoa // Operador de desestruturação
+console.log(`${nome} - ${idade} anos.`)
+
+const { nome: n, idade: i} = pessoa
+console.log(`${n} - ${i} anos.`)
+
+const { sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
