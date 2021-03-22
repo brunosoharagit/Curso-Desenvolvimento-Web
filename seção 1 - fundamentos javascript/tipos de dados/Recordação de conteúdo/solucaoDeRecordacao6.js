a254f5e3410e091799e82031493c0a40aefd8508
brunosoharagit <b.sohara.git@gmail.com>
2021-03-22 10:14:07 -0300

    CDSV2021.03.22.10.13 - solucaoDeRecordacao6.js: criação e inserção de exemplos

---
+++ seção 1 - fundamentos javascript/tipos de dados/Recordação de conteúdo/solucaoDeRecordacao6.js
@@ -0,0 +1,12 @@
+const notebook = {
+    processador: 'Intel Core i5 - 9900U',
+    placaGrafica: 'GeForce GTX 1050ti',
+    acessorios: {
+        teclado: 'Teclado HyperX Neon C456',
+        mousepad: 'Mousepad Razer Chroma',
+        mouse: 'Mouse Gamer HyperX Cryon X333' 
+    }
+}
+
+const { processador, placaGrafica } = notebook
+console.log(processador)
