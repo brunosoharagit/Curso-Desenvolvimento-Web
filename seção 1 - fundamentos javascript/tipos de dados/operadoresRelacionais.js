6e4e5010738642141f22adb9e1de6dabdda2ee0f
brunosoharagit <b.sohara.git@gmail.com>
2021-03-22 14:44:27 -0300

    CDSV2021.03.22.14.43 - operadoresRelacionais.js: inserindo casos relacionais

---
+++ seção 1 - fundamentos javascript/tipos de dados/operadoresRelacionais.js
@@ -0,0 +1,2 @@
+console.log('01)', '1' == 1) // no primeiro momento está comparando o valor
+console.log('02)', '1' === 1) // neste segundo momento, está sendo comparado o tipo de valor (string e int)
