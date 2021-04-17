c13f40a5399d6c3f6b5beedd5a552a53ade4944b
brunosoharagit <b.sohara.git@gmail.com>
2021-04-17 10:46:54 -0300

    CDSV2021.04.17.10.47 - general: removendo pasta 'revisao'

--- revisao/revisao1.js
+++
@@ -1,9 +0,0 @@
-const aluno = {
-    nome: 'Bruno', 
-    datanasc: '21/08/1996',
-    anoLetivo: '5º Semestre',
-    disciplinas: ['DWEL5', 'SSRL5', 'MCPL4', 'ED2L5']
-}
-
-const { nome: n, datanasc: dn } = aluno
-console.log(`${n} - Data de nascimento: ${dn}`);
