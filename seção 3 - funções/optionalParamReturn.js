3dfccafdc3f8eed52d1c749012274a4888882b5a
brunosoharagit <b.sohara.git@gmail.com>
2021-05-01 11:56:21 -0300

    CDSV 2021.05.01 - optionalParamsReturn.js

--- seção 3 - funções/optionalParamReturn.js
+++ seção 3 - funções/optionalParamReturn.js
@@ -14,3 +14,17 @@ console.log(area(3))
 console.log(area())
 console.log(area(3, 6, 9, 12, 15))
 console.log(area(3, 30))
+
+function soma(){
+    let soma = 0 
+    for (i in arguments){
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1, 2))
+console.log(Math.ceil(soma(1.1, 2.2, 3.3)));
+console.log(soma(' - B','r','u','n','o'))
