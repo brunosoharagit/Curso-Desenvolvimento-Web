fdf98a4c95c1308571f3dc79e60beb9ac68a5210
brunosoharagit <b.sohara.git@gmail.com>
2021-05-01 12:04:12 -0300

    CDSV 2021.05.01 - paramPadrao.js

--- seção 3 - funções/paramPadrao.js
+++ seção 3 - funções/paramPadrao.js
@@ -10,9 +10,9 @@ console.log(soma1(), soma1(3), soma1(3, 3, 2)), soma1(0, 0, 0);
 
 //Estratégia 2, 3 e 4 para geração de valores padrão
 function soma2(a, b, c){
-    a = a !== undefined ? a : 1
-    b = 1 in arguments ? b : 1
-    c = isNaN(c) ? 1 : c
+    a = a !== undefined ? a : 1 //(2)
+    b = 1 in arguments ? b : 1 //(3)
+    c = isNaN(c) ? 1 : c //(4)
     return a + b + c
 }
 
@@ -26,9 +26,9 @@ function soma3(a = 1, b = 1, c = 1){
 console.log(soma3(), soma3(2), soma3(1, 2, 3), soma3(0, 0, 0))
 
 //Exemplo real
-function inseriuNome(nome){
-    nome = nome || 'Anônimo'
-    return nome
+function inseriuNome(nickname){
+    nickname = nickname || `Desconhecido`
+    return nickname
 }
 
-console.log(inseriuNome(), inseriuNome('Yotyan'));
+console.log(inseriuNome(), inseriuNome('Yotyan'), inseriuNome('Adder V'));
