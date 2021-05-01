a3a290acc88e0a557b49a297ba4ce2f0a007e642
brunosoharagit <b.sohara.git@gmail.com>
2021-05-01 14:27:23 -0300

    CDSV 2021.05.01 - Arrow Funcrions

---
+++ seção 3 - funções/arrowFunction1.js
@@ -0,0 +1,16 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {return 2 * a}
+
+dobro = a => 2 * a //return 
+console.log(dobro(Math.PI));
+
+let ola = function() {return 'Olá!'}
+
+ola = () => 'Olá'
+//ou
+ola = _ => 'Olá' //possui um parametro
+
+console.log(ola());
