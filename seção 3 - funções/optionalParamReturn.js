5824a280d7fc5402608e6fdefa049064fdecc893
brunosoharagit <b.sohara.git@gmail.com>
2021-05-01 11:40:29 -0300

    CDSV 2021.05.01 - optionalParamReturn.js

--- seção 3 - funções/optionalParamReturn.js
+++ seção 3 - funções/optionalParamReturn.js
@@ -1,7 +1,7 @@
 function area(altura, largura){
     const area = largura*altura
     if (area > 20){
-        return `Área maior do que o permitido. Área inserida: ${area}m².`
+        return `Área maior do que a permitida. Área inserida: ${area}m².`
     }
     else {
         return area
@@ -12,5 +12,5 @@ function area(altura, largura){
 console.log(area(3, 3))
 console.log(area(3))
 console.log(area())
-console.log(area(3, 3, 3, 3, 3))
+console.log(area(3, 6, 9, 12, 15))
 console.log(area(3, 30))
