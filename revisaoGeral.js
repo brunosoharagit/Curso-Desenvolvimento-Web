7afb031e4a7590a4afdcbab497b186390c511077
brunosoharagit <b.sohara.git@gmail.com>
2021-05-01 11:28:55 -0300

    CDSV 2021.05.01 - revisaoGeral.js

--- revisaoGeral.js
+++ revisaoGeral.js
@@ -1,3 +1,27 @@
 function fun1(){
-    
+    return 'Olá mundo'
+}
+
+const fun2 = function(){
+    return 'Olá mundo'
+}
+
+console.log(fun1());
+console.log(fun2());
+
+const obj = {}
+obj.falar = function() { return 'Opa!!' }
+
+console.log(obj.falar());
+
+function run(func){
+    func()
+}
+
+run(function() { console.log('Executando função. . .') })
+
+function soma(a, b){
+    return function(c){
+        console.log(a + b + c);
+    }
 }
